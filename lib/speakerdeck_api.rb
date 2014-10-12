6c4fa6ff442099f3209a47601e6cfaca288dfc25
Fer Perales <me@ferperales.net>
2014-10-12 00:53:22 -0500

    Add missing require

--- lib/speakerdeck_api.rb
+++ lib/speakerdeck_api.rb
@@ -1,4 +1,8 @@
 require "speakerdeck_api/version"
+require 'speakerdeck_api/connection'
+require 'speakerdeck_api/parser'
+require 'speakerdeck_api/speaker'
+require 'speakerdeck_api/exceptions/speaker_not_found_exception'
 
 module SpeakerdeckApi
   # Your code goes here...
